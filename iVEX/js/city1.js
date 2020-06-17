9135305386f36e127beed9c2fde847e9d2835a48
TanayNaik19 <62985321+TanayNaik19@users.noreply.github.com>
2020-06-17 16:40:32 +0530

    Created

---
+++ iVEX/js/city1.js
@@ -0,0 +1,23 @@
+// NAV BAR
+$(".button-collapse").sideNav();
+$(".dropdown-trigger").dropdown();
+
+
+// DROPDOWN
+$('.dropdown-trigger').dropdown();
+
+// SLIDER
+$('.slider').slider();
+
+// TABS
+$('.tabs').tabs();
+
+// COLLAPSIBLE
+$('.collapsible').collapsible();
+
+// CAROUSEL
+$('.carousel').carousel();
+
+// DATEPICKER
+$('.datepicker').datepicker();
+          
---
+++ iVEX/js/city2.js
@@ -0,0 +1,22 @@
+// NAV BAR
+$(".button-collapse").sideNav();
+$(".dropdown-trigger").dropdown();
+
+
+// DROPDOWN
+$('.dropdown-trigger').dropdown();
+
+// SLIDER
+$('.slider').slider();
+
+// TABS
+$('.tabs').tabs();
+
+// COLLAPSIBLE
+$('.collapsible').collapsible();
+
+// CAROUSEL
+$('.carousel').carousel();
+
+// DATEPICKER
+$('.datepicker').datepicker();
